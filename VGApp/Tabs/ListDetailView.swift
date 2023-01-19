80aa3debbd209f5e5cc79eab6e5064096eacd690
DragonCat - Kiara <68660034+DragonCat4012@users.noreply.github.com>
2023-01-19 19:15:18 +0100

    listdetailview

---
+++ VGApp/Tabs/ListDetailView.swift
@@ -0,0 +1,30 @@
+//
+//  ListDetailView.swift
+//  VGApp
+//
+//  Created by Kiara on 19.01.23.
+//
+
+import SwiftUI
+
+
+struct ListDetail: View {
+    @State var list: ShoppingList
+    
+    var body: some View {
+            VStack{
+                Spacer()
+                Text("Inhalt")
+                List {
+                    ForEach(Util.getItems(list)!) { item in
+                        HStack{
+                            Text(item.name ?? "Namenlos")
+                            Spacer()
+                            Text(item.number ?? "####").foregroundColor(.gray)
+                        }
+                    }
+                }
+              
+            }
+    }
+}
