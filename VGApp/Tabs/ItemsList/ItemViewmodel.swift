e27d7976f24b3fd8a2da3860a53d8468d2fd2141
Kiara <68660034+DragonCat4012@users.noreply.github.com>
2023-07-14 14:24:58 +0200

    fix #4
    itemlist view swipe actions

--- VGApp/Tabs/ItemsList/ItemViewmodel.swift
+++ VGApp/Tabs/ItemsList/ItemViewmodel.swift
@@ -15,39 +15,68 @@ enum SHEETTYPE {
 class ItemViewmodel: ObservableObject {
     @Published var list: ShoppingList?
     @Published var items: [Item] = []
-
+    
     @Published var showsSheet = false
     @Published var selectedSheetType = SHEETTYPE.newItem
-
+    
     // new Item
     @Published var newName = ""
     @Published var newNumber = ""
-
+    
     // new recycle
     @Published var newRecycleType = 0
     @Published var typeArr: [RecycleTypes] = []
+    @Published var usedRecycleTypes: [RecycleTypes] = []
+    @Published var recycleDict: [RecycleTypes: Int] = [:]
     
     @Published var showDeleteAlert = false
-
+    
     func updateViews() {
         withAnimation {
             self.objectWillChange.send()
             list = Util.getSelectedList()
             items = Util.getItems()
+            typeArr = RecycleTypes.allCases
+            setUsedRecyleTypesArr()
         }
-        typeArr = RecycleTypes.allCases
     }
-
-    func deleteItems() {
-        guard let list = list else {
-            return
+    
+    func setUsedRecyleTypesArr() {
+        guard let list = list else { return }
+        usedRecycleTypes = []
+        let dict = CoreData.getRecylcesDict(list)
+        
+        RecycleTypes.allCases.forEach { type in
+            if dict[type] != 0 { usedRecycleTypes.append(type) }
+            recycleDict[type] = dict[type]
         }
+    }
+    
+    func deleteItems() {
+        guard let list = list else { return }
+        
         Util.deleteAllItems(list)
         Util.save()
         updateViews()
     }
-
+    
+    func removeRecyle(for type: RecycleTypes) {
+        guard let list = list else { return }
+        CoreData.removeRecycle(list, type: type)
+        Util.save()
+        setUsedRecyleTypesArr()
+    }
+    
+    func addRecyle(for type: RecycleTypes) {
+        guard let list = list else { return }
+        CoreData.addRecycle(list, type: type)
+        Util.save()
+        setUsedRecyleTypesArr()
+    }
+    
     func removeItems(at offsets: IndexSet) {
+        print("called remove")
+        
         for i in offsets.makeIterator() {
             let theItem = items[i]
             CoreData.removeItem(theItem, list!)
@@ -55,24 +84,24 @@ class ItemViewmodel: ObservableObject {
         }
         updateViews()
     }
-
+    
     func showNewRecycleSheet() {
         selectedSheetType = .newRecycle
         withAnimation { showsSheet = true }
     }
-
+    
     func showNewItemSheet() {
         selectedSheetType = .newItem
         withAnimation { showsSheet = true }
     }
-
+    
     func addRecyle() {
         let newRecyle = typeArr[newRecycleType]
         CoreData.addRecycle(list!, type: newRecyle)
         Util.save()
         withAnimation { showsSheet = false }
     }
-
+    
     func createItem() {
         Util.createItem(name: newName, code: newNumber)
         updateViews()
